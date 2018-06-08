bb84aef4b13c192ec50f93adee1699c2296143d7
Jonathan Hosein <jonathan.hosein@live.com>
2018-06-08 23:18:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
 
-  if (cardNumber === "undefined") {
+  if (typeof cardNumber === "undefined") {
     return "Sorry we don't have a credit card on file for you.";
   }
   else {
