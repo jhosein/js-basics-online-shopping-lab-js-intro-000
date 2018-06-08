608e0d7d92edb76f37000beab8e4586f2f3c76fe
Jonathan Hosein <jonathan.hosein@live.com>
2018-06-08 23:17:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,11 +70,11 @@ function placeOrder(cardNumber) {
     return "Sorry we don't have a credit card on file for you.";
   }
   else {
-    var tot = total();
+    var orderTotal = total();
 
     cart = [];
 
-    let message = `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+    let message = `Your total cost is $${orderTotal}, which will be charged to the card ${cardNumber}.`;
     return message;
   }
 }
